174efc88980569c882ab0394efb283fc590fadc3
TyRoXx <tyroxxxx@gmail.com>
2013-05-26 22:35:00 +0200

    simplify macros

--- src/rxn/struct2.hpp
+++ src/rxn/struct2.hpp
@@ -8,12 +8,10 @@
 
 #define RXN_FIELD(rxn_name, rxn_type, rxn_annotations) rxn_type rxn_name;
 
-#define RXN_GET_0(a) BOOST_PP_SEQ_ELEM(0, a)
-#define RXN_GET_1(a) BOOST_PP_SEQ_ELEM(1, a)
-#define RXN_GET_2(a) BOOST_PP_SEQ_ELEM(2, a)
-
 #define RXN_PARSE_FIELD(r, data, i, e) \
-	RXN_FIELD(RXN_GET_0(e), RXN_GET_1(e), RXN_GET_2(e))
+	RXN_FIELD(BOOST_PP_SEQ_ELEM(0, e), \
+			  BOOST_PP_SEQ_ELEM(1, e), \
+			  BOOST_PP_SEQ_ELEM(2, e))
 
 #define RXN_FIELDS(...) \
 	BOOST_PP_SEQ_FOR_EACH_I( \
