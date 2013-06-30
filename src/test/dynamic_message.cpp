2f32ae9a44cfed30e4e5f735b61cfe209c3e26f4
TyRoXx <tyroxxxx@gmail.com>
2013-06-30 20:39:56 +0200

    test for future dynamic message APIs

---
+++ src/test/dynamic_message.cpp
@@ -0,0 +1,43 @@
+#include <boost/test/unit_test.hpp>
+
+#include <szn/bytes.hpp>
+#include <szn/big_endian.hpp>
+#include <szn/bool.hpp>
+
+namespace szn
+{
+	namespace
+	{
+	}
+
+	BOOST_AUTO_TEST_CASE(Serialization_dynamic_message)
+	{
+		std::string generated;
+		auto sink = makeContainerSink(generated);
+
+		//this should be possible with less typing:
+		szn::serialize(sink, 12, szn::BE32());
+		szn::serialize(sink, "hello", szn::Bytes<szn::BE16>());
+		szn::serialize(sink, true, szn::Bool());
+
+		/* //ideas:
+		szn::writer(sink)
+			.be32(12)
+		    .be<32>(12)
+			.bytes16("hello")
+		    .utf8<16>("hello")
+			.boolean(true)
+		    .bool_(true)
+			;
+		*/
+
+		char const expectedBytes[] =
+		{
+			0, 0, 0, 12,
+		    0, 5, 'h', 'e', 'l', 'l', 'o',
+		    1
+		};
+		BOOST_CHECK_EQUAL(generated,
+		                  std::string(expectedBytes, sizeof(expectedBytes)));
+	}
+}
