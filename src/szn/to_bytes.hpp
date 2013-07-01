e0d821cf82943735b7f5dbb34e4736250b4c590e
TyRoXx <tyroxxxx@gmail.com>
2013-07-01 20:24:17 +0200

    header for to_bytes helper

---
+++ src/szn/to_bytes.hpp
@@ -0,0 +1,41 @@
+#ifndef SERIALIZATION_TO_BYTES_HPP
+#define SERIALIZATION_TO_BYTES_HPP
+
+
+#include <cstring>
+#include <vector>
+
+
+namespace szn
+{
+	inline std::pair<char const *, std::size_t>
+	to_bytes(std::string const &str)
+	{
+		return std::make_pair(str.data(), str.size());
+	}
+
+	template <class C>
+	typename std::enable_if<sizeof(C) == 1, std::pair<char const *, std::size_t>>::type
+	to_bytes(std::vector<C> const &vec)
+	{
+		return std::make_pair(
+					reinterpret_cast<char const *>(vec.data()),
+					vec.size());
+	}
+
+	inline std::pair<char const *, std::size_t>
+	to_bytes(char const *c_str)
+	{
+		return std::make_pair(c_str, std::strlen(c_str));
+	}
+
+	template <class C, std::size_t N>
+	typename std::enable_if<sizeof(C) == 1, std::pair<char const *, std::size_t>>::type
+	to_bytes(C (&arr)[N])
+	{
+		return std::make_pair(reinterpret_cast<char const *>(&arr[0]), N);
+	}
+}
+
+
+#endif
--- src/szn/writer.hpp
+++ src/szn/writer.hpp
@@ -3,45 +3,12 @@
 
 
 #include <szn/sink.hpp>
+#include <szn/to_bytes.hpp>
 #include <boost/cstdint.hpp>
-#include <boost/range/size.hpp>
-#include <cstring>
-#include <vector>
 
 
 namespace szn
 {
-	namespace detail
-	{
-		inline std::pair<char const *, std::size_t>
-		to_bytes(std::string const &str)
-		{
-			return std::make_pair(str.data(), str.size());
-		}
-
-		template <class C>
-		typename std::enable_if<sizeof(C) == 1, std::pair<char const *, std::size_t>>::type
-		to_bytes(std::vector<C> const &vec)
-		{
-			return std::make_pair(
-			            reinterpret_cast<char const *>(vec.data()),
-			            vec.size());
-		}
-
-		inline std::pair<char const *, std::size_t>
-		to_bytes(char const *c_str)
-		{
-			return std::make_pair(c_str, std::strlen(c_str));
-		}
-
-		template <class C, std::size_t N>
-		typename std::enable_if<sizeof(C) == 1, std::pair<char const *, std::size_t>>::type
-		to_bytes(C (&arr)[N])
-		{
-			return std::make_pair(reinterpret_cast<char const *>(&arr[0]), N);
-		}
-	}
-
 	struct writer SZN_FINAL
 	{
 		explicit writer(sink &destination);
@@ -53,7 +20,6 @@ namespace szn
 		template <class LengthFormat, class ByteContainer>
 		writer &bytes(ByteContainer const &value)
 		{
-			using detail::to_bytes;
 			auto const bytes = to_bytes(value);
 			LengthFormat().serialize(m_destination, bytes.second);
 			return raw(bytes.first, bytes.second);
