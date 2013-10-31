aad7c9c7c1ce064183dc526f49ddf38c01ab441b
TyRoXx <tyroxxxx@gmail.com>
2013-10-31 20:30:39 +0100

    use szn::bytes instead of char in all to_bytes overloads

--- src/szn/to_bytes.hpp
+++ src/szn/to_bytes.hpp
@@ -16,14 +16,6 @@ namespace szn
 		return range;
 	}
 
-	template <class C, class Traits, class Allocator>
-	boost::iterator_range<char const *>
-	to_bytes(std::basic_string<C, Traits, Allocator> const &str)
-	{
-		return boost::make_iterator_range(str.data(),
-		                                  str.data() + str.size());
-	}
-
 	namespace detail
 	{
 		//boost::enable_if_c<sizeof(C) == 1 ..> does not work for VC++ 2008,
@@ -32,6 +24,14 @@ namespace szn
 		struct is_char : boost::integral_constant<bool, sizeof(C) == 1>
 		{
 		};
+	}
+
+	template <class C, class Traits, class Allocator>
+	typename boost::enable_if<detail::is_char<C>, boost::iterator_range<byte const *> >::type
+	to_bytes(std::basic_string<C, Traits, Allocator> const &str)
+	{
+		byte const *begin = reinterpret_cast<byte const *>(str.data());
+		return boost::make_iterator_range(begin, begin + str.size());
 	}
 
 	template <class C, class Allocator>
@@ -44,18 +44,19 @@ namespace szn
 					begin + vec.size());
 	}
 
-	inline boost::iterator_range<char const *>
+	inline boost::iterator_range<byte const *>
 	to_bytes(char const *c_str)
 	{
-		return boost::make_iterator_range(c_str,
-	                                      c_str + std::strlen(c_str));
+	    byte const *begin = reinterpret_cast<byte const *>(c_str);
+		return boost::make_iterator_range(begin,
+	                                      begin + std::strlen(c_str));
 	}
 
 	template <class C, std::size_t N>
-	typename boost::enable_if_c<sizeof(C) == 1, boost::iterator_range<char const *>>::type
+	typename boost::enable_if_c<sizeof(C) == 1, boost::iterator_range<byte const *> >::type
 	to_bytes(C (&arr)[N])
 	{
-	    char const * const begin = reinterpret_cast<char const *>(&arr[0]);
+	    byte const * const begin = reinterpret_cast<byte const *>(&arr[0]);
 		return boost::make_iterator_range(begin, begin + N);
 	}
 }
