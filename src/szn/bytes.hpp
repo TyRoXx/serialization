a51e5fd9c5bed685c11d740b98398939927bbf12
TyRoXx <tyroxxxx@gmail.com>
2013-10-31 21:04:55 +0100

    remove unused includes

--- src/szn/bytes.hpp
+++ src/szn/bytes.hpp
@@ -2,20 +2,8 @@
 #define SERIALIZATION_BYTES_HPP_8BE0A18D_7A18_41A3_B154_C6C47903EDE7
 
 
-#include <szn/util.hpp>
-#include <szn/format.hpp>
-#include <szn/to_bytes.hpp>
 #include <szn/vector.hpp>
 #include <szn/big_endian.hpp>
-#include <boost/static_assert.hpp>
-#include <boost/type_traits/is_pointer.hpp>
-#include <boost/utility.hpp> //next
-#include <boost/cstdint.hpp>
-#include <string>
-#include <vector>
-#include <utility>
-#include <stdexcept>
-#include <cassert>
 
 
 namespace szn
